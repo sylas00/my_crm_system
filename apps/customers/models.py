a5cd7b871dfaedce199f964a77a123da15928697
slowclimb <1207104511@qq.com>
2021-05-26 11:36:20 +0800

    完善省份三级联合表

--- apps/customers/models.py
+++ apps/customers/models.py
@@ -5,36 +5,30 @@ from shops.models import ShopModel
 from users.models import User
 
 
+# 自关联的表 导入的时候 一级类目的parent如果是-1 这样的 会报错 因为找不到主键为-1的行 所以设置为允许null 直接把一级类目的parent设置成null
 class AreaModel(models.Model):
-    """
-    行政区划
-    null = True：（必写项）允许省级的父级为空
-    blank = True：（必写项）约束将来在admin站点
-    parent表单里可以不填
-    on_delete：删除守护，比如将来需要删除某个市，如果不做守护，会把这个市下面的区全都删掉
-    """
-    name = models.CharField(max_length=20, verbose_name='名称')
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='subs', blank=True,
-                               verbose_name='上一级行政区')
-
-    def __str__(self):
-        return self.name
+    id = models.CharField(primary_key=True, max_length=10)
+    name = models.CharField(max_length=50, verbose_name='名称')
+    short_name = models.CharField(max_length=10, blank=True, null=True, verbose_name='简称')
+    code = models.CharField(max_length=20, blank=True, null=True, verbose_name='邮编')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, verbose_name='上一级行政区')
+    level = models.IntegerField(blank=True, null=True, verbose_name='级别', )
 
     class Meta:
-        db_table = 'crm_areas'
+        db_table = 'crm_area'
         verbose_name = '行政区划'
         verbose_name_plural = verbose_name
 
 
 class CustomerModel(BaseModel):
     name = models.CharField(max_length=255, verbose_name='客户名')
-    create_account = models.ForeignKey(User, on_delete=models.CASCADE,verbose_name='创建人')
+    create_account = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='创建人')
     shop = models.ForeignKey(ShopModel, on_delete=models.CASCADE, verbose_name='客户所属店铺')
     qq = models.CharField(max_length=255, blank=True, verbose_name='QQ')
     wechat = models.CharField(max_length=255, blank=True, verbose_name='微信')
     phone = models.CharField(max_length=11, blank=True, verbose_name='手机号')
     tel = models.CharField(max_length=255, blank=True, verbose_name='座机电话')
-    address = models.ForeignKey(AreaModel, on_delete=models.CASCADE, blank=True,verbose_name='地址')
+    address = models.ForeignKey(AreaModel, on_delete=models.CASCADE, blank=True, verbose_name='地址')
 
     # 操作记录 投诉 举报
 
