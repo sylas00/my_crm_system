7a9fb89a79ba3f8ce553d0b50463de11f2302daf
slowclimb <1207104511@qq.com>
2021-05-26 17:11:05 +0800

    完成支付方式视图

--- apps/payment/serializer.py
+++ apps/payment/serializer.py
@@ -1,1 +1,8 @@
 from . import models
+from rest_framework import serializers
+
+
+class PaymentSer(serializers.ModelSerializer):
+    class Meta:
+        model = models.PaymentModel
+        fields = '__all__'
--- apps/payment/views.py
+++ apps/payment/views.py
@@ -1,3 +1,8 @@
 from rest_framework import viewsets
 from . import models
 from . import serializer
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = models.PaymentModel.objects.all()
+    serializer_class = serializer.PaymentSer
